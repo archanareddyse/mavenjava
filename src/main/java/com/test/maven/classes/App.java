b4eb02c6b4a733ad43852f588bdf8eca07de1314
archanareddyse <101937605+archanareddyse@users.noreply.github.com>
2023-12-06 09:41:57 +0530

    Update App.java

--- src/main/java/com/test/maven/classes/App.java
+++ src/main/java/com/test/maven/classes/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hellllllo World!" );
+        System.out.println( "Hellllllo Worlllllllllllld!" );
     }
 }
